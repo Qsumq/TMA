06544a2ccd1a1a7a7012aaf8d6d2dfe4578dfbcd
Qsumq <103766386+Qsumq@users.noreply.github.com>
2024-06-10 00:57:44 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-function greetUser() {
-  var nameInput = document.getElementById("nameInput");
-  var greetingElement = document.getElementById("greeting");
-
-  var name = nameInput.value.trim();
-  if (name) {
-    var greeting = "Hello, " + name + "!";
-    greetingElement.textContent = greeting;
-  } else {
-    greetingElement.textContent = "Please enter your name.";
-  }
+function displayPrice(price) {
+  var priceDisplayElement = document.getElementById("priceDisplay");
+  priceDisplayElement.value = "$" + price;
 }
